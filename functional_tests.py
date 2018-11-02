e373e8ef0f2c247453fe87028b38b2c5b23644eb
Raymund Martinez <zhaqenl@gmail.com>
2018-11-02 18:47:48 +0800

    Use unittest module inside FT

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_ask_user_to_browse_for_file(self):
+        # Ram goes to check the homepage of the new website
+        self.browser.get('http://localhost:8000')
+
+        # He notices the title of the browser tab
+        self.assertIn('Kingdom in Django', self.browser.title)
+        self.fail('You hit the simulated failure.')
+        
+        # Ram is then invited to browse for a specially structured .txt file
+
+        # The button text changes from 'Browse' to 'Upload' after selecting the file
+
+        # Ram then clicks the button to upload the file. After the successful file upload, he is
+        # taken to a new page which shows the processed stats of the file
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
