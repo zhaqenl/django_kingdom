1e72fc316297a2286ba388f4d036cdd677d123dd
Raymund Martinez <zhaqenl@gmail.com>
2018-11-25 13:05:01 +0800

    Pass minimal html test

--- kingdom/tests.py
+++ kingdom/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from kingdom.views import home_page
 
 class SmokeTest(TestCase):
@@ -7,3 +9,11 @@ class SmokeTest(TestCase):
     def test_home_page_resolving(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Kingdom in Django</title>', html)
+        self.assertTrue(html.endswith('</html>'))
